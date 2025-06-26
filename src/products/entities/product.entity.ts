55b2c6cbd9f7c6d4eaa8295448dbdad387d0744b
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-25 23:36:50 -0400

    feat: actualizar producto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,3 +1,9 @@
+interface UpdateWithOptions {
+  name?: string;
+  description?: string;
+  price?: number;
+}
+
 export class Product {
   id: string;
   name: string;
@@ -9,5 +15,11 @@ export class Product {
     this.name = name;
     this.description = description;
     this.price = price;
+  }
+
+  updateWith({ name, description, price }: UpdateWithOptions) {
+    this.name = name ?? this.name;
+    this.description = description ?? this.description;
+    this.price = price ?? this.price;
   }
 }
