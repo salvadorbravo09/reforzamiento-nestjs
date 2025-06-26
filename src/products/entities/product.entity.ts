ca4f3da700e2fe5c0f21d0dab476d1632604aae4
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-25 22:21:36 -0400

    feat: listar y eliminar productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -4,7 +4,7 @@ export class Product {
   description?: string;
   price: number;
 
-  constructor(id: string, name: string, description?: string, price: number) {
+  constructor(id: string, name: string, price: number, description?: string) {
     this.id = id;
     this.name = name;
     this.description = description;
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
@@ -11,24 +11,31 @@ export class ProductsService {
   create(createProductDto: CreateProductDto) {
     const { name, description, price } = createProductDto;
 
-    const newProduct = new Product(UuidV4(), name, description, price);
+    const newProduct = new Product(UuidV4(), name, price, description);
     this.products.push(newProduct);
     return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Product {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product with id '${id}' not found`);
+    }
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string): Product {
+    const product = this.findOne(id);
+    // Devuelve todos los productos que no coinciden con el id del producto a eliminar
+    this.products = this.products.filter((product) => product.id !== id);
+    return product;
   }
 }
