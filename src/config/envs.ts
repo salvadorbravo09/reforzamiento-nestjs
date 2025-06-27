d863b9920bfa89b1ee2e9bcd9a7d678b2847401d
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-26 22:08:39 -0400

    feat: joi - esquema de validacion

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,5 +1,25 @@
 import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env) as {
+  error?: joi.ValidationError;
+  value: EnvVars;
+};
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
 
 export const envs = {
-  port: process.env.PORT,
+  port: value.PORT,
 };
